ad1944716f39a5f1b0cf0d301cecd1fa72187db5
syedsaifali <syedsaifali214@gmail.com>
2025-04-06 11:34:50 +0530

    deploy.sh added

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/app
+
+echo "Installing dependencies..."
+npm install
+
+echo "Stopping old server..."
+pkill -f "node server.js" || true
+
+echo "Starting server.js in background..."
+nohup node server.js > server.log 2>&1 &
